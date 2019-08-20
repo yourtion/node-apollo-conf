fe14b12225ec2d659be533c657fc4ae1c764f5f1
YourtionGuo <yourtion@gmail.com>
2019-08-20 13:26:52 +0800

    docs: update config

--- src/url.ts
+++ src/url.ts
@@ -4,6 +4,8 @@ import { createQuery } from "./utils";
  * URL生成与构建
  * @link https://github.com/ctripcorp/apollo/wiki/%E5%85%B6%E5%AE%83%E8%AF%AD%E8%A8%80%E5%AE%A2%E6%88%B7%E7%AB%AF%E6%8E%A5%E5%85%A5%E6%8C%87%E5%8D%97
  */
+
+/** 基础配置 */
 export interface IQueryConfig {
   // Apollo配置服务的地址
   url: string;
